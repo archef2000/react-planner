4a2464885acbd721ea894cba56e0b3fd778cc85a
archef2000 <69057532+archef2000@users.noreply.github.com>
2025-08-29 14:13:56 +0000

    test message

--- src/components/sidebar/panel.tsx
+++ src/components/sidebar/panel.tsx
@@ -66,7 +66,7 @@ export default class Panel extends Component<PanelProps, PanelState> {
 
     return (
       <div style={STYLE}>
-        <p>wrong 3</p>
+        <p>wrong (create message)</p>
         <h3
           style={{ ...STYLE_TITLE, color: hover ? SharedStyle.SECONDARY_COLOR.main : SharedStyle.PRIMARY_COLOR.text_alt }}
           onMouseEnter={() => this.toggleHover()}
